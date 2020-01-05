24be4c7d1947a7ddaca686b6f652c2ad6c3d8f81
EdZeno <eduard.kulcsickij.gut@gmail.com>
2020-01-05 11:51:15 +0000

    it returns buzz

--- Fizz/fizz.py
+++ Fizz/fizz.py
@@ -1,3 +1,5 @@
 def fizz_buzz(number):
   if number % 3 == 0:
     return 'Fizz'
+  elif number % 5 == 0:
+    return 'Buzz'
--- tests/test_FizzBuzz.py
+++ tests/test_FizzBuzz.py
@@ -2,3 +2,6 @@ from Fizz.fizz import fizz_buzz
 
 def test_return_fizz():
     assert fizz_buzz(3) == "Fizz"
+
+def test_return_buzz():
+    assert fizz_buzz(5) == 'Buzz'
